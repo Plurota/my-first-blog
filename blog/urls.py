6e6b4d1eea3b647d8a3be1c92135b7991cd0ada8
Plurota <kagancelikm@gmail.com>
2021-11-06 22:50:04 +0300

    Site için CSS ve ayrıntılı blog gönderisi için görünüm ve şablon eklendi.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.shortcuts import get_object_or_404
 
 urlpatterns = [
 	path('', views.post_list, name='post-list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
